98e1e91b6fe963b54d84fb96ae2eb1b4744d0506
poledna0 <henrique.poledna@gmail.com>
2024-04-04 08:56:04 -0300

    feat: nsei

--- forca.py
+++ forca.py
@@ -8,42 +8,33 @@ LetrasLista = []
 Ganhou = False
 
 while True:
-    for Letra in PalavraParaAdivinhar:
-        LetraAdivinhada = False
-        for LetraEscolhida in LetrasLista:
-            if LetraEscolhida.lower() == Letra.lower():
-                LetraAdivinhada = True
-                break
+    TamanhoPalavra = len(PalavraParaAdivinhar)
+    Posicao = 0
 
-        if LetraAdivinhada:
-            print(Letra, end=' ')
+    while Posicao < TamanhoPalavra:
+        letra = PalavraParaAdivinhar[Posicao]
+
+        if letra in LetrasLista:
+            print(letra.lower(), end=' ')
         else:
             print('_', end=' ')
+            
+        Posicao += 1
 
-    ChuteDeLetra = input('Escolha uma letra para tentar completar a palavra: ').lower()
-    
-    LetrasLista.append(ChuteDeLetra)
-    Ganhou = True
+    ChuteDeLetra = input('Escolha uma letra para tentar completar a palavra: ')
+    print(f'Você tem mais {ChancesParaAcertar} chances')
 
-    for letra in PalavraParaAdivinhar:
-        LetraAdivinhada = False
-        for LetraEscolhida in LetrasLista:
-            if LetraEscolhida.lower() == letra.lower():
-                LetraAdivinhada = True
-                break
-        if not LetraAdivinhada:
-            Ganhou = False
-            break
+    if ChuteDeLetra.lower() not in PalavraParaAdivinhar.lower():
+        ChancesParaAcertar -= 1
+    else:
+        LetrasLista.append(ChuteDeLetra.lower())
 
-    if Ganhou:
+    if set(PalavraParaAdivinhar.lower()) == set(LetrasLista):
+        Ganhou = True
         break
 
     if ChancesParaAcertar == 0:
         break
-
-    if ChuteDeLetra not in PalavraParaAdivinhar.lower():
-        ChancesParaAcertar -= 1
-        print(f'Você tem mais {ChancesParaAcertar} chances')
 
 if Ganhou:
     print(f'Parabéns, você ganhou o jogo! A palavra era "{PalavraParaAdivinhar}".')
