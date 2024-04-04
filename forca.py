8731888286cc911577023d3eae47c4e2c7e15474
poledna0 <henrique.poledna@gmail.com>
2024-04-04 08:59:03 -0300

    feat: 2

--- forca.py
+++ forca.py
@@ -1,35 +1,27 @@
 PalavraParaAdivinhar = input('Digite a palavra para ser adivinhada: ')
 ChancesParaAcertar = int(input('Digite a quantidade de vezes que pode errar: '))
 
-# para limpar o terminal
-print('\033c', end='')
+# para limpar o teminal
+print('\033c', end='') 
 
 LetrasLista = []
 Ganhou = False
 
 while True:
-    TamanhoPalavra = len(PalavraParaAdivinhar)
-    Posicao = 0
-
-    while Posicao < TamanhoPalavra:
-        letra = PalavraParaAdivinhar[Posicao]
-
-        if letra in LetrasLista:
-            print(letra.lower(), end=' ')
+    for Letras in PalavraParaAdivinhar:
+        if Letras in LetrasLista:
+            print(Letras.lower(), end=' ')
         else:
             print('_', end=' ')
-            
-        Posicao += 1
 
     ChuteDeLetra = input('Escolha uma letra para tentar completar a palavra: ')
+    LetrasLista.append(ChuteDeLetra.lower())
     print(f'Você tem mais {ChancesParaAcertar} chances')
 
-    if ChuteDeLetra.lower() not in PalavraParaAdivinhar.lower():
-        ChancesParaAcertar -= 1
-    else:
-        LetrasLista.append(ChuteDeLetra.lower())
+    if ChuteDeLetra.lower() not in PalavraParaAdivinhar.lower(): # n pode
+        ChancesParaAcertar -= 1 
 
-    if set(PalavraParaAdivinhar.lower()) == set(LetrasLista):
+    if set(PalavraParaAdivinhar.lower()) == set(LetrasLista):  #perguntar para o prof
         Ganhou = True
         break
 
@@ -37,6 +29,6 @@ while True:
         break
 
 if Ganhou:
-    print(f'Parabéns, você ganhou o jogo! A palavra era "{PalavraParaAdivinhar}".')
+    print(f'Parabéns, você ganhou o jogo! A palavra era "{PalavraParaAdivinhar}".') 
 else:
     print(f'Você perdeu o jogo! A palavra era "{PalavraParaAdivinhar}".')
